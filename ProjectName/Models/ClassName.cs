39a0353fb5b5de74af71ddf2606e9efc8c137599
Jamila Aliyeva <aliyeva852@gmail.com>
2019-12-20 07:55:43 -0800

    create project dir.

--- ProjectName/Models/ClassName.cs
+++ ProjectName/Models/ClassName.cs
@@ -7,67 +7,19 @@ namespace Project.Models
 {
   public class Change
   {
-      public int Quarters; 
-      public int Dimes;  
-      public int Nickles;   //ask why code doesnt work when get/set are added
-      public int Pennies;  
-      public double Total;  
-
+      
   
-    public Change(double total, int quarters, int dimes, int nickles, int pennies)
+    public ( )
     {
-        Quarters = quarters;
-        Dimes = dimes;
-        Nickles = nickles;
-        Pennies = pennies;
-        Total = total;
+       
     }
-        public int getQuarters()
-        {
-            return Quarters;
-        }
-
-        public int getDimes()
-        {
-            return Dimes;
-        }
-        public int getNickles()
-        {
-            return Nickles;
-        }
-
-        public int getPennies()
-        {
-            return Pennies;
-        }
-    
-
+       
         
-        public void ChangeConverter(double total)
+        public void ( )
         {
-            while (total > 0)
-            { 
-                if (total >= 0.25)
-                {
-                  total -= 0.25;
-                  Quarters ++;
-                }
-                if(total >= 0.10)
-                {
-                    total -= 0.10;
-                    Dimes++;
-                }
-                if(total >= 0.05)
-                {
-                    total -= 0.05;
-                    Nickles++;
-                }
-                if  (total >= 0.01); 
-                {
-                    total -= 0.01;
-                    Pennies++;
-                }
-            }
+            
+                
+            
         }
     }
   }
--- ProjectName/Models/Project.cs
+++ ProjectName/Models/Project.cs
@@ -9,14 +9,7 @@ namespace Project.Models
     {
          public static void Main()
         {
-            Console.WriteLine("Enter amount you want");
-            int userInput = int.Parse(Console.ReadLine());
-            Change user = new Change(userInput,0,0,0,0);
-            user.ChangeConverter(userInput);
-            Console.WriteLine("Quarters" + "" + user.getQuarters());
-            Console.WriteLine("Dimes" + "" +user.getDimes());
-            Console.WriteLine("Nickes" +user.getNickles());
-            Console.WriteLine("Pennies" + "" +user.getPennies());
+           
         }
     }
 }

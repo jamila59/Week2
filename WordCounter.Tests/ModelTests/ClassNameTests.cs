b7afcbbf8f07e48bb2ebace75432406776201b8d
Jamila Aliyeva <aliyeva852@gmail.com>
2019-12-20 09:25:57 -0800

    run tests.

--- WordCounter.Tests/ModelTests/ClassNameTests.cs
+++ WordCounter.Tests/ModelTests/ClassNameTests.cs
@@ -1,13 +1,27 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ToDoList.Models;
+using System.Collections.Generic;
+using Counter.Models;
 
-namespace ToDoList.Tests
+
+namespace Counter.Tests
 {
   [TestClass]
   public class ItemTest
-  {
-
-    // Test methods will go here.
-
+  { 
+    [TestMethod]
+    public void WordConstructor_CreatesInstance_Word()
+    {
+      Term example = new Term("string", "string of words");
+      Assert.AreEqual(typeof(Term), example.GetType());
+    }
+    [TestMethod]
+    public void Match_ChangesToUpper_String()
+    {
+      string up = "string";
+      string up2 = "string of words";
+      Term example  = new Term(up , up2);
+      string result = example.Match(up, up2);
+      Assert.AreEqual(up2, result);
+    }
   }
 }

2c7d63aee78375888c41aaab40b72913d81aedeb
Jamila Aliyeva <aliyeva852@gmail.com>
2019-12-20 15:59:01 -0800

    fix bug with string entry, align console dislplay and add lisence.

--- WordCounter/Models/Project.cs
+++ WordCounter/Models/Project.cs
@@ -12,24 +12,29 @@ namespace Project.Models
         {
            Console.WriteLine("Enter a sentence:");
            string userSentence = Console.ReadLine();
-           if (string.IsNullOrEmpty(userSentence) == true || userSentence != "" )
+           if (string.IsNullOrEmpty(userSentence) == true )
            {
                 Console.WriteLine("Invalid input, try again.");
                 Console.WriteLine("Enter a sentence:");
                 userSentence = Console.ReadLine();
                 
-            }       
+            }   
+            Console.WriteLine("---------------------------------");
             Console.WriteLine("Enter a word:");
             string userWord = Console.ReadLine();
-            if (string.IsNullOrEmpty(userWord) == true || userSentence != "")
+            if (string.IsNullOrEmpty(userWord) == true )
             {
                 Console.WriteLine("Invalid input, try again.");
                 Console.WriteLine("Enter a word:");
+                Console.WriteLine("---------------------------------");
                 userWord = Console.ReadLine();
             }
             Term user = new Term(userWord, userSentence);
             user.RepeatCounter(userWord, userSentence);
+            Console.WriteLine("---------------------------------");
             Console.WriteLine(" "+ userWord + "  appears  " + user.getScore()+ "  " + "time(s) in the sentence.");
+            Console.WriteLine("---------------------------------");
         }
     }
-}
+}   
+

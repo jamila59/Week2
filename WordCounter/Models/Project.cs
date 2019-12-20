11ddb3fd6e680034d6ad5a3a350e056775b9034e
Jamila Aliyeva <aliyeva852@gmail.com>
2019-12-20 08:20:52 -0800

    create specs for project and change project name.

---
+++ WordCounter/Models/Project.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Project.Models
+{
+    class Program
+    {
+         public static void Main()
+        {
+           
+        }
+    }
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Project.Models
+{
+  public class Change
+  {
+      
+  
+    public ( )
+    {
+       
+    }
+       
+        
+        public void ( )
+        {
+            
+                
+            
+        }
+    }
+  }
+ 
+  
